456505214c03c7719384f2339aa0fe4135a40b5a
Stevan Vlajic <stevanvlajic5@gmail.com>
2023-02-19 21:21:03 +0100

    signup regex live-validation

--- components/signup.php
+++ components/signup.php
@@ -27,10 +27,91 @@ if (
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    
-    $file_dest = "not defined" ;
+    echo $name . ": " . $username . ", " . $lastname . ", " . $email . ", " . $password;
+
+    $dbserver = "sensorify.ddns.net";
+    $dbname = "sensorifydb";
+    $dbusername = "stevan";
+    $dbpassword = "Stevan2006";
+    $connection = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+
+    if (!exists($username, $connection)) {
+        if (!$connection) {
+            die("Connection failed: " . mysqli_connect_errno());
+        }
+        #uploads selected file if user does not exist
+        upload_File();
+        $sql = "insert into users (name,lastname,user_name,email,password,image_dest)"
+            . "values('$name','$lastname','$username','$email','$password','$file_dest')"; # filedest
+        if (mysqli_query($connection, $sql)) {
+            $last_id = mysqli_insert_id($connection);
+            echo "Successfully inserted ID: " . $last_id;
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        $_SESSION['login'] = true;
+    } else {
+        echo "<br>";
+        echo "$username already exists";
+        $_SESSION['login'] = false;
+        header('Location: home.php');
+    }
+    mysqli_close($connection);
+} else {
+
+    if (!$_SESSION['login']) {
+        echo "
+        <form id='signup' action='./home.php' method='post' enctype='multipart/form-data'>
+        <h3 id='login_headline'>Sign up</h3>
+        <div id='signup-grid'>
+            <div id='input-box'>
+                <input type='text' id='username' class='input' name='username' placeholder='User Name' required>
+                <br>
+                <input type='text' id='name' class='input' name='name' placeholder='First Name' required>
+                <br>
+                <input type='text' id='lastname' class='input' name='lastname' placeholder='Last Name' required>
+                <br>
+                <input type='email' id='email' class='input' name='email' placeholder='Email Address' required>
+                <br>
+                <input type='password' id='password' class='input' name='password' placeholder='Password' required>
+                <br>
+            </div>
+            <div id='image-selector'> 
+            <img src='./img/plus.svg' id='profile_image' class='file_image' alt='profile_image' onclick='triggerClick()'>
+                <label for='fileimage'></label>
+                <input onchange='displayImage(this)' id='fileimage' type='file' name='file' accept='image/png, image/jpg, image/svg, image/jpeg'/> 
+            </div>
+        </div>
+        <input type='submit' id='submit' name='submit'>
+    </form>
+";
+    }
+}
+
+
+# check if data base contains-same username
+function exists($username, $connection)
+{
+    $sql = "select user_name from users where user_name='$username'";
+
+    echo $username;
+    $result = $connection->query($sql);
+
+    echo $result->num_rows;
+
+    if ($result->num_rows == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function upload_File(){
+    $file_dest = "not defined";
     # read image methods
     $file = $_FILES['file']; # output -> array ['name'], [type]...
+
+    # $file-> propeteries
     $file_name = $_FILES['file']['name'];
     $file_tmp_name = $_FILES['file']['tmp_name']; # temp location of the file
     $file_size = $_FILES['file']['size']; # size of the file
@@ -47,7 +128,7 @@ if (
         if ($file_error === 0) {
             if ($file_size <= 5_000_000_0) { # max iamge size
                 $file_name_new = uniqid('', true) . "." . $file_actual_ext; # image unique name for overriding purposes
-                $file_dest = './upload/' . $file_name_new; 
+                $file_dest = './upload/' . $file_name_new;
                 # move temp file loaction to new location
                 move_uploaded_file($file_tmp_name, $file_dest);
                 header("Location: home.php?uploadsuccess");
@@ -59,82 +140,5 @@ if (
         }
     } else {
         echo "You cannot upload files of this type...";
-    }
-
-    echo $name . ": " . $username . ", " . $lastname . ", " . $email . ", " . $password;
-
-    $dbserver = "sensorify.ddns.net";
-    $dbname = "sensorifydb";
-    $dbusername = "stevan";
-    $dbpassword = "Stevan2006";
-    $connection = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
-
-
-    if (!exists($username, $connection)) {
-        if (!$connection) {
-            die("Connection failed: " . mysqli_connect_errno());
-        }
-
-        $sql = "insert into users (name,lastname,user_name,email,password,image_dest)"
-            . "values('$name','$lastname','$username','$email','$password','$file_dest')"; // filedest
-
-        if (mysqli_query($connection, $sql)) {
-            $last_id = mysqli_insert_id($connection);
-            echo "Successfully inserted ID: " . $last_id;
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
-        $_SESSION['login'] = true;
-    } else {
-        echo "<br>";
-        echo "$username already exists";
-        $_SESSION['login'] = false;
-    }
-
-    mysqli_close($connection);
-} else {
-
-    if (!$_SESSION['login']) {
-        echo "
-        <form id='signup' action='./home.php' method='post' enctype='multipart/form-data'>
-        <h3 id='login_headline'>Sign up</h3>
-        <div id='signup-grid'>
-            <div id='input-box'>
-                <input type='text' class='input' name='username' placeholder='User Name' required>
-                <br>
-                <input type='text' class='input' name='name' placeholder='First Name' required>
-                <br>
-                <input type='text' class='input' name='lastname' placeholder='Last Name' required>
-                <br>
-                <input type='email' class='input' name='email' placeholder='Email Address' required>
-                <br>
-                <input type='text' class='input' name='password' placeholder='Password' required>
-                <br>
-            </div>
-            <div id='image-selector'> 
-            <img src='./img/image-search.svg' id='profile_image' class='file_image' alt='profile_image' onclick='triggerClick()'>
-                <label for='fileimage'></label>
-                <input onchange='displayImage(this)' id='fileimage' type='file' name='file' accept='image/png, image/jpg, image/svg, image/jpeg'/> 
-            </div>
-        </div>
-        <input type='submit' id='submit' name='submit'>
-    </form>
-";
-    }
-}
-
-function exists($username, $connection)
-{
-    $sql = "select user_name from users where user_name='$username'";
-
-    echo $username;
-    $result = $connection->query($sql);
-
-    echo $result->num_rows;
-
-    if ($result->num_rows == 0) {
-        return false;
-    } else {
-        return true;
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,114 @@ function displayImage(e) {
 
         reader.onload = function(e) {
             document.querySelector('#profile_image').setAttribute('src', e.target.result);
+            document.getElementById('profile_image').style.opacity="1";
         }
         reader.readAsDataURL(e.files[0]);
+    }
+}
+
+
+// regex checking input form
+let signup = document.getElementById('signup');
+if(typeof(signup) != "undefined" && signup != null){
+    // disable button --> enable after successfull regex-check 
+    const sendButton = document.getElementById("submit");
+    sendButton.disabled = true;
+
+    // At least 8 characters long
+    // Contains at least one lowercase letter
+    // Contains at least one uppercase letter
+    // Contains at least one digit
+    // Contains at least one special character (one of !@#$%^&*()_+)
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+
+    // At least 3 characters long
+    // At most 16 characters long
+    // Contains only alphanumeric characters (letters and numbers) and underscores
+    const usernameRegex = /^[a-zA-Z0-9_]{3,16}$/;
+
+    // The address starts with one or more word characters, hyphens, or periods ([\w-\.]+).
+    // This is followed by an at sign (@).
+    // After the at sign, there is a domain name consisting of one or more groups of word characters or hyphens, separated by periods (([\w-]+\.)+), followed by a top-level domain (TLD) consisting of two to four word characters ([\w-]{2,4}).
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    // The name can start with one or more alphabetical characters (^[a-zA-Z]+).
+    // The name can have any number of middle names or initials, each separated by a space, hyphen, apostrophe, comma, or period (([',. -][a-zA-Z ])?).
+    // The last name is required and can have any number of alphabetical characters ([a-zA-Z]*$).
+    const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+
+    let username = document.getElementById('username');
+    let name = document.getElementById('name');
+    let lastname = document.getElementById('lastname');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+
+    username.addEventListener('keyup', validateUsername);
+    name.addEventListener('keyup', validateName);
+    lastname.addEventListener('keyup', validateLastname);
+    email.addEventListener('keyup', validateEmail);
+    password.addEventListener('keyup', validatePassword);
+
+    setInterval(() => {
+        if(
+            validateUsername() &&
+            validateName() &&
+            validateLastname() && 
+            validateEmail() && 
+            validatePassword()
+        ){
+            // regex check successfull --> button accessed
+            sendButton.disabled = false;
+        }
+    }, 100);
+    function validateUsername() {
+        let isValid = usernameRegex.test(username.value);
+        if(!isValid){
+            username.style.borderBlockColor = "#e30a0a77";
+            return false;
+        }else{
+            username.style.borderBlockColor = "#1a876667";
+            return true;
+        }
+    }
+    function validateName() {
+        let isValid = nameRegex.test(name.value);
+        if(!isValid){
+            name.style.borderBlockColor = "#e30a0a77";
+            return false;
+        }else{
+            name.style.borderBlockColor = "#1a876667";
+            return true;
+        }
+    }
+    function validateLastname() {
+        let isValid = nameRegex.test(lastname.value);
+        if(!isValid){
+            lastname.style.borderBlockColor = "#e30a0a77";
+            return false;
+        }else{
+            lastname.style.borderBlockColor = "#1a876667";
+            return true;
+        }
+    }
+    function validateEmail() {
+        let isValid = emailRegex.test(email.value);
+        if(!isValid){
+            email.style.borderBlockColor = "#e30a0a77";
+            return false;
+        }else{
+            email.style.borderBlockColor = "#1a876667";
+            return true;
+        }
+    }
+    function validatePassword() {
+        let isValid = nameRegex.test(password.value);
+        if(!isValid){
+            password.style.borderBlockColor = "#e30a0a77";
+            return false;
+        }else{
+            password.style.borderBlockColor = "#1a876667";
+            return true;
+        }
     }
 }
