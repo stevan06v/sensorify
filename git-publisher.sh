c5876c283efeb94f41ad76e6acfb866f44b3e940
Stevan Vlajic <stevanvlajic5@gmail.com>
2023-03-26 19:10:38 +0200

    test

--- git-publisher.sh
+++ git-publisher.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 chmod 777 git-publisher.sh
-
 # clear images in ./app/upload/
-rm -r "/app/upload/*.png"
-
-# publish to github 
+rm -r ./app/upload/*
+# publish to github
 git add .
 git commit -m "$1"
 git push
